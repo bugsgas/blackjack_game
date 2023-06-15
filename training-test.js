83ed39323f376a56c4814d7ee9594fc983c31e1f
Macintosh <macintosh@Macintoshs-MacBook-Air.local>
2023-06-15 17:13:04 +0700

    first commit

---
+++ training-test.js
@@ -0,0 +1,60 @@
+
+
+// let hands = ["rock", "paper", "scissor"]
+
+// function randomHand() {
+//     let randomIndex = Math.floor(Math.random() * 3)
+//     return hands[randomIndex]
+// }
+
+// console.log(randomHand())
+
+// let dayOfMonth = 31
+// let weekday ="Friday"
+
+// if (dayOfMonth === 13 && weekday === "Friday") {
+//     console.log("Scream")
+// }
+
+
+// let largeCountries = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"]
+
+// largeCountries.pop()
+// largeCountries.push("Pakistan")
+// largeCountries.shift()
+// largeCountries.unshift("China")
+
+// console.log(largeCountries)
+
+// let largerCountries = ["China", "India", "USA", "Indonesia", "Pakistan"]
+
+// for (let i = 0; i < largerCountries.length; i++){
+//     console.log("- " + largerCountries[i])
+// }
+
+// let age = 18
+// if (age < 6){
+//     return "Free"
+// } else if (age < 19){
+//     console.log("child discount") 
+// } else if ( age < 27 ){
+//     console.log("student discount")
+// } else if (age < 67){
+//     console.log("full price")
+// } else {
+//     console.log("senior citizen discount")
+// }
+
+// console.log(age)
+
+// let person = {
+//     name: "bagas",
+//     age: 12,
+//     country: "Indonesia"
+// }
+
+// function logData(){
+//     return person.name + " is " + person.age + " years old and lives in " + person.country
+// }
+
+// console.log(logData())
